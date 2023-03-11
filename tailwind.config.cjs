c0c51e06a60f793d9c2cc4156177aeb727f67e96
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-03-11 10:22:34 -0800

    Minor changes to Login and Signup with Hero changes in images and text.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'blue1': '#004a8d',
+        'red1': '#F01114'
+      },
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
